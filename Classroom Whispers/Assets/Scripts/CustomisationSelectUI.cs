691f0999f30a6fcf45ed13a854ca585c41fcf264
Owen Wigg <oj.wigg@gmail.com>
2018-01-28 15:12:47 +1100

    Final commit for the build. \o/

---
+++ Classroom Whispers/Assets/Scripts/CustomisationSelectUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomisationSelectUI : MonoBehaviour , IPointerClickHandler
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+        Debug.Log(name + " Game Object Clicked!");
+    }
+}
---
+++ Classroom Whispers/Assets/Scripts/SceneProgresser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneProgresser : MonoBehaviour {
+
+    public string SceneName = "";
+    public int AfterCountdown = 4;
+
+    private float _timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _timer += Time.deltaTime;
+        if ((int)_timer >= AfterCountdown)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+
+    }
+}
