e50f00fcd85fbcd1828ad6f766236218ce934985
Owen Wigg <oj.wigg@gmail.com>
2018-01-28 12:41:19 +1100

    Added remaning SFX Added ability to have random ambiences playing

--- Classroom Whispers/Assets/Scripts/AudioManager.cs
+++ Classroom Whispers/Assets/Scripts/AudioManager.cs
@@ -23,11 +23,54 @@ public class RandomSFX
     }
 }
 
+[System.Serializable]
+public class ReapeatingAmbient
+{    
+    public string AmbientName;
+    public string SFXName;
+    public float MinTimePeriod;
+    public float MaxTimePeriod;
+    public int PlayLimit = 0;
+
+    [System.NonSerialized]
+    private float _timer;
+
+    [System.NonSerialized]
+    private int _count;
+
+    public void SetNextTimer()
+    {
+        _timer = Random.Range(MinTimePeriod, MaxTimePeriod);
+    }
+
+    public void Update()
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                Play();
+                if (PlayLimit == 0 || _count < PlayLimit)
+                    SetNextTimer();
+            }
+        }
+    }
+
+    public void Play()
+    {
+        AudioManager.Instance.PlayAmbientSFX(SFXName);
+        _count++;
+    }
+}
+
 public class AudioManager : MonoBehaviour {
 
     public static AudioManager Instance { get; private set; }
 
-    public RandomSFX[] SFXGroups;    
+    public RandomSFX[] SFXGroups;
+
+    public List<ReapeatingAmbient> RepeatingAmbients = new List<ReapeatingAmbient>();
 
     private AudioSource _ambientSource;
     private AudioSource _actionSource;
@@ -45,12 +88,32 @@ public class AudioManager : MonoBehaviour {
         {
             _lookup.Add(sfx.SFXName, sfx);
         }
+
+        foreach (var amb in RepeatingAmbients)
+        {
+            amb.SetNextTimer();
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        foreach (var amb in RepeatingAmbients)
+        {            
+            amb.Update();
+        }
+    }
+    
+    void AddNewRepeatingAmbient(string key, string sfxName, float minTimer, float maxTimer, int limit=0)
+    {
+        ReapeatingAmbient amb = new ReapeatingAmbient();
+        amb.AmbientName = key;
+        amb.SFXName = sfxName;
+        amb.MinTimePeriod = minTimer;
+        amb.MaxTimePeriod = maxTimer;
+        amb.PlayLimit = limit;
+
+        RepeatingAmbients.Add(amb);
+    }
 
     private RandomSFX GetSFX(string sfxName)
     {
